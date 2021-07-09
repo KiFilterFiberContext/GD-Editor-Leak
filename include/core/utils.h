c92ba0bc00c81b126c05c983d8636b410199f8f9
encls <51222153+encls@users.noreply.github.com>
2021-07-09 16:24:55 -0700

    Update utils.h

--- include/core/utils.h
+++ include/core/utils.h
@@ -1,55 +1,41 @@
 #pragma once
 
-#include <android/log.h>
-
-namespace saber::logging 
+namespace photon::utils
 {
-    static constexpr bool DEBUG_ENABLED = true;
-    static constexpr auto DEBUG_TAG = "**Saber**";
+	constexpr bool debug_log = true;
 
-    enum log_type 
-    {
-        unknown,
-        normal, // DEFAULT
-        verbose,
-        debug,
-        info,
-        warn,
-        error,
-        fatal,
-        silent
-    };
+	template <typename... Args>
+	static inline void debug_print( Args... args )
+	{
+		if constexpr ( !debug_log )
+			return;
 
-    /* FOREGROUND */
-    #define RST  "\x1B[0m"
-    #define KRED  "\x1B[31m"
-    #define KGRN  "\x1B[32m"
-    #define KYEL  "\x1B[33m"
-    #define KBLU  "\x1B[34m"
-    #define KMAG  "\x1B[35m"
-    #define KCYN  "\x1B[36m"
-    #define KWHT  "\x1B[37m"
+		__android_log_print( ANDROID_LOG_DEBUG, "[DEBUG] photon -- ", args... );
+	}
 
-    #define FRED(x) KRED x RST
-    #define FGRN(x) KGRN x RST
-    #define FYEL(x) KYEL x RST
-    #define FBLU(x) KBLU x RST
-    #define FMAG(x) KMAG x RST
-    #define FCYN(x) KCYN x RST
-    #define FWHT(x) KWHT x RST
+	template <typename... Args>
+	static inline void print( Args... args )
+	{
+		__android_log_print( ANDROID_LOG_INFO, "photon -- ", args... );
+	}
 
-    #define BOLD(x) "\x1B[1m" x RST
-    #define UNDL(x) "\x1B[4m" x RST
+	static bool str_replace( std::string& str, const std::string& from, const std::string& to )
+	{
+		size_t start_pos = str.find( from );
+		if ( start_pos == std::string::npos )
+			return false;
+		str.replace( start_pos, from.length( ), to );
+		return true;
+	}
 
-    template <log_type T = log_type::debug, typename... Args>
-    inline void log( const char* fmt, Args... args )
-    {
-        if constexpr ( !DEBUG_ENABLED )
-            return;
-    
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wformat-security"
-    __android_log_print( T, DEBUG_TAG, fmt, args... );
-#pragma clang diagnostic pop
-    }
+	typedef struct handle
+	{
+		Elf64_Ehdr* ehdr;
+		Elf64_Phdr* phdr;
+		Elf64_Shdr* shdr;
+		uint8_t* mem;
+		char* symname;
+		Elf64_Addr symaddr;
+		char* exec;
+	} handle_t;
 }
