884779ecdf57451271092437d959d94ca21242b3
encls <51222153+encls@users.noreply.github.com>
2021-07-09 16:19:56 -0700

    Update FLAlertLayer.h

--- include/gd/FLAlertLayer.h
+++ include/gd/FLAlertLayer.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "CCLayer.h"
+#include "CCMenu.h"
+#include "CCNode.h"
+
+#include "ButtonSprite.h"
 
 class FLAlertLayerProtocol { };
 
@@ -8,12 +12,30 @@ class FLAlertLayerProtocol { };
 class FLAlertLayer : public cocos2d::CCLayerColor
 {
 public:
+    cocos2d::CCMenu* btnMenu_;
+    int controlConnected_;
+    FLAlertLayerProtocol* PParent_;
+    cocos2d::CCNode* targetScene_;
+    bool reverseKeyBack_;
+    cocos2d::ccColor3B color_;
+    cocos2d::CCLayer* internalLayer_;
+    int ZValue_;
+    bool showInstant_;
+    cocos2d::ccColor3B color2_;
+    ButtonSprite* btn1_;
+    ButtonSprite* btn2_;
+    void* scrollingLayer_;
+    int joystickConnected_;
+    bool containsActions_;
+    bool noBtnAction_;
+
+public:
     FLAlertLayer();
     virtual ~FLAlertLayer();
 
     static FLAlertLayer* create( FLAlertLayerProtocol* protocol, const char* title, std::string caption, const char* button_title, const char* button2_title );
     static FLAlertLayer* create( FLAlertLayerProtocol* protocol, const char* title, std::string caption, const char* b1_title, const char* b2_title, float x_size, bool add_border, float y_size );
-    
+
     virtual void placeholder1();
     virtual void placeholder2();
 
