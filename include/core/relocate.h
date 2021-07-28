f4b6027779feed061e116c4121c50b7e28e990f3
NtTuna <51222153+NtTuna@users.noreply.github.com>
2021-07-28 12:16:27 -0700

    Add files via upload

---
+++ include/core/relocate.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+
+void relocateInstruction(uint32_t target_addr, void *orig_instructions, int length, void *trampoline_instructions, int *orig_boundaries, int *trampoline_boundaries, int *count);
