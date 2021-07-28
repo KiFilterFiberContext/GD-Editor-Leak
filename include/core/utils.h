08929423b69c25afb47d4e996f343ffa042281b4
NtTuna <51222153+NtTuna@users.noreply.github.com>
2021-07-28 12:17:34 -0700

    fix some stuff

--- include/core/utils.h
+++ include/core/utils.h
@@ -28,6 +28,24 @@ namespace photon::utils
 		return true;
 	}
 
+	template <typename T>
+	static int do_inline_hook( const char* sym, T replacement, T* old )
+	{
+		uintptr_t address = ( uintptr_t ) get_lib_symbol( sym );
+		if ( address == 0 )
+			return -1;
+
+		const auto reg_result = registerInlineHook( address, ( uint32_t ) replacement, ( uint32_t** ) old ); // &old
+		if ( reg_result != ELE7EN_OK )
+			return -2;
+
+		const auto hook_result = inlineHook( address );
+		if ( hook_result != ELE7EN_OK )
+			return -3;
+
+		return 0;
+	}
+
 	typedef struct handle
 	{
 		Elf64_Ehdr* ehdr;
